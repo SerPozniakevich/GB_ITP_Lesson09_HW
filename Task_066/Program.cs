143cff2d488c9f9352fbe934dc90fd763892561b
Sergey Pozniakevich <ven07@yandex.ru>
2022-09-15 19:54:50 +0700

    add Task 066 solution

---
+++ Task_066/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу,
+//которая найдёт сумму натуральных элементов в промежудке от M до N. 
+
+Console.Clear();
+
+
+System.Console.WriteLine("Enter M: ");
+int start = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Enter N: ");
+int end = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"M = {start}; N = {end}. -> {AmountLineNumbers(start, end)}");
+
+
+int AmountLineNumbers(int start, int end)
+{
+return (end == start) ? end: end + AmountLineNumbers (start, end - 1);
+}
